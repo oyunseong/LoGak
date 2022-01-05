7cb18ebc9553a969005c6f91b9b5c8a6a03cbe86
Administrator <gkrlsanjf@naver.com>
2022-01-05 14:11:11 +0900

    SplashActivity -> MainActivity 로 이동할 때 애니메이션 추가

--- app/src/main/java/com/oys/logak/SplashActivity.kt
+++ app/src/main/java/com/oys/logak/SplashActivity.kt
@@ -7,19 +7,21 @@ import android.os.Handler
 import com.oys.logak.base.BaseActivity
 import com.oys.logak.databinding.ActivitySplashBinding
 
-class SplashActivity : BaseActivity<ActivitySplashBinding>({ ActivitySplashBinding.inflate(it)}){
+class SplashActivity : BaseActivity<ActivitySplashBinding>({ ActivitySplashBinding.inflate(it) }) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Handler().postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
             startActivity(intent)
             finish()
+            overridePendingTransition(R.anim.slide_fade_in, R.anim.slide_fade_out)
         }, DURATION)
     }
-    companion object{
-        private const val DURATION : Long = 1000
+
+    companion object {
+        private const val DURATION: Long = 1000
     }
 
     override fun onBackPressed() {
