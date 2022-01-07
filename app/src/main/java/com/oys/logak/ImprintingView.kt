45536ff426d8a408b3f38736236c151b06921e4b
Administrator <gkrlsanjf@naver.com>
2022-01-08 01:51:21 +0900

    Log 추가

--- app/src/main/java/com/oys/logak/ImprintingView.kt
+++ app/src/main/java/com/oys/logak/ImprintingView.kt
@@ -14,16 +14,20 @@ class ImprintingView : ConstraintLayout {
 
     // 코드에서 View 객체를 생성할 때 호출하는 생성자
     constructor(context: Context) : super(context) {
+        Log.d("++ImprintingView", "1번 생성자")
         initView()
     }
 
+    // 얘가 호출됨!
     // 레이아웃 xml에 등록한 View가 안드로이드에 의해 inflate될 때 호출되는 생성자
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        Log.d("++ImprintingView", "2번 생성자")
         initView()
         getAttrs(attrs)
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs) {
+        Log.d("++ImprintingView", "3번 생성자")
         initView()
         getAttrs(attrs, defStyle)
     }
@@ -35,11 +39,13 @@ class ImprintingView : ConstraintLayout {
      * View Binding으로 레이아웃 파일 연결
      */
     private fun initView() {
+        Log.d("++ImprintingView","initView")
         binding = ViewImprintingBinding.inflate(LayoutInflater.from(context), this, true)
 
     }
 
     override fun onDraw(canvas: Canvas?) {
+        Log.d("++ImprintingView","showDraw")
         super.onDraw(canvas)
     }
 
@@ -47,18 +53,21 @@ class ImprintingView : ConstraintLayout {
      * attrs.xml에 선언해둔 attribute를 이용하여 이를 각각의 View에 설정해준다.
      */
     private fun getAttrs(attrs: AttributeSet) {
+        Log.d("++ImprintingView","getAttrs")    // 얘가 불림
         val typedArray =
             context.obtainStyledAttributes(attrs, R.styleable.ImprintingView)
         setTypedArray(typedArray)
     }
 
     private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
+        Log.d("++ImprintingView","getAttrs2")
         val typedArray =
             context.obtainStyledAttributes(attrs, R.styleable.ImprintingView, defStyle, 0)
         setTypedArray(typedArray)
     }
 
     private fun setTypedArray(typedArray: TypedArray) {
+        Log.d("++ImprintingView","setTypedArray")
         val bgResId = typedArray.getResourceId(R.styleable.ImprintingView_bgColor, R.color.white)
         binding.imprintingBg.setBackgroundResource(bgResId)
 
@@ -66,20 +75,19 @@ class ImprintingView : ConstraintLayout {
             typedArray.getResourceId(R.styleable.ImprintingView_symbol, R.drawable.blank)
         binding.blank1.setImageResource(symbolResId)
         binding.blank2.setImageResource(symbolResId)
-//        binding.blank3.setImageResource(symbolResId)
-//        binding.blank4.setImageResource(symbolResId)
-//        binding.blank5.setImageResource(symbolResId)
-//        binding.blank6.setImageResource(symbolResId)
-//        binding.blank7.setImageResource(symbolResId)
-//        binding.blank8.setImageResource(symbolResId)
-//        binding.blank9.setImageResource(symbolResId)
-//        binding.blank10.setImageResource(symbolResId)
-//        binding.blank11.setImageResource(symbolResId)
-//        binding.blank12.setImageResource(symbolResId)
-//        binding.blank13.setImageResource(symbolResId)
-//        binding.blank14.setImageResource(symbolResId)
-//        binding.blank15.setImageResource(symbolResId)
-
+        binding.blank3.setImageResource(symbolResId)
+        binding.blank4.setImageResource(symbolResId)
+        binding.blank5.setImageResource(symbolResId)
+        binding.blank6.setImageResource(symbolResId)
+        binding.blank7.setImageResource(symbolResId)
+        binding.blank8.setImageResource(symbolResId)
+        binding.blank9.setImageResource(symbolResId)
+        binding.blank10.setImageResource(symbolResId)
+        binding.blank11.setImageResource(symbolResId)
+        binding.blank12.setImageResource(symbolResId)
+        binding.blank13.setImageResource(symbolResId)
+        binding.blank14.setImageResource(symbolResId)
+        binding.blank15.setImageResource(symbolResId)
 
         val textColor = typedArray.getColor(R.styleable.ImprintingView_textColor, 0)
         binding.imprintingText.setTextColor(textColor)
@@ -92,10 +100,10 @@ class ImprintingView : ConstraintLayout {
         binding.imprintingText.text = text
         binding.imprintingLevelText.text = levelText
         binding.imprintingOverCountText.text = overCountText
-        // 로그 잘 들어옴
-        Log.d("imprintingView.kt", "" + text)
-        Log.d("imprintingView.kt", "" + levelText)
-        Log.d("imprintingView.kt", "" + overCountText)
+
+        Log.d("++ImprintingView", "" + text)
+        Log.d("++ImprintingView", "" + levelText)
+        Log.d("++ImprintingView", "" + overCountText)
 
         typedArray.recycle()
     }
@@ -105,8 +113,8 @@ class ImprintingView : ConstraintLayout {
         binding.imprintingLevelText.text = levelText
         binding.imprintingOverCountText.text = overCountText
 
-        Log.d("imprintingView.kt", "setText: " + text)
-        Log.d("imprintingView.kt", "setText: " + levelText)
-        Log.d("imprintingView.kt", "setText: " + overCountText)
+        Log.d("++ImprintingView", "setText: " + text)
+        Log.d("++ImprintingView", "setText: " + levelText)
+        Log.d("++ImprintingView", "setText: " + overCountText)
     }
 }
