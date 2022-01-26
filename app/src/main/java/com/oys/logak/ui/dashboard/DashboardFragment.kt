d933e40ca3edbaa77b1c8c47ba6da988eed51f7e
Administrator <gkrlsanjf@naver.com>
2022-01-26 15:51:10 +0900

    commit

--- app/src/main/java/com/oys/logak/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/oys/logak/ui/dashboard/DashboardFragment.kt
@@ -1,45 +1,18 @@
 package com.oys.logak.ui.dashboard
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.oys.logak.R
+import com.oys.logak.base.BaseFragment
 import com.oys.logak.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
-
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
+class DashboardFragment : BaseFragment<FragmentDashboardBinding>() {
+    override fun getFragmentBinding(
         inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-
-        })
-        return root
+        container: ViewGroup?
+    ): FragmentDashboardBinding {
+        return FragmentDashboardBinding.inflate(inflater,container,false)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+
+
 }
